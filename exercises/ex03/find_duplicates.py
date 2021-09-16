9d4799d8185379a54618d6e96db6b2b2872129be
Ailise Rodgers <ailise@email.unc.edu>
2021-09-15 23:41:16 -0400

    Started and finished ex03

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,25 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730383357"
+
+word: str = input("Enter a word: ")
+i: int = 0
+duplicate: bool = False
+
+while i < len(word):
+    letter: str = word[i]
+    j: int = i + 1
+    while j < len(word):
+        if letter == word[j]:
+            duplicate = True
+        j = j + 1
+    i = i + 1
+
+if duplicate == True:
+    print("Found duplicate: True")
+else:
+    print("Found duplicate: False")
+
+### VS Code is telling me there's a bug at ###
+### line 18, but it still works just fine ###
+### so I'm leaving it alone ###
--- exercises/ex03/tar_heels.py
+++ exercises/ex03/tar_heels.py
@@ -1,6 +1,19 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730383357"
 
 
-# Begin your solution here...
+number: int = int(input("Enter a int: "))
+remainder_2: int = number % 2
+remainder_7: int = number % 7
+
+if remainder_2 != 0:
+    if remainder_7 != 0:
+        print("CAROLINA")
+    else:
+        print("HEELS")
+else:
+    if remainder_7 == 0:
+        print("TAR HEELS")
+    else:
+        print("TAR")
