5779ce51052a41cee7788c4aca93da3f5161f512
Ailise Rodgers <ailise@email.unc.edu>
2021-09-29 22:08:06 -0400

    Well, let's see if this works

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,42 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730383357"
 
 
-# TODO: Implement your functions here.
+def all(long: list[int], short: int) -> bool:
+    """Return True if all values of long match short, False otherwise."""
+    i: int = 0
+    if len(long) > 0:
+        while i < len(long):
+            if long[i] != short:
+                return False
+            i += 1
+        return True
+    else:
+        return False
+
+
+def is_equal(a: list[int], b: list[int]) -> bool:
+    """Returns True if a and b are deeply equal, False otherwise."""
+    i: int = 0
+    if len(a) == len(b):
+        while i < len(a) and i < len(b):
+            if a[i] != b[i]:
+                return False
+            i += 1
+        return True
+    else:
+        return False
+
+
+def max(large: list[int]) -> int:
+    i: int = 0
+    if len(large) == 0:
+        raise ValueError("max() arg is an empty List")
+    else:
+        value: int = large[0]
+        while i < len(large):
+            if value < large[i + 1]:
+                value: int = large[i + 1]
+            i += 1
+        return value
