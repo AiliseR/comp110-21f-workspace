b7a751f3a173e3b3600a829704856912be47bbba
Ailise Rodgers <ailise@email.unc.edu>
2021-11-03 00:22:52 -0400

    do I know what I'm doing? somewhat

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -1,5 +1,77 @@
 """Utility functions."""
 
-__author__ = "123456789"
+__author__ = "730383357"
 
-# Define your functions below
+from csv import DictReader
+
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read the rows of a csv into a 'table'."""
+    result: list[dict[str, str]] = []
+
+    # Open a handle to the data file
+    file_handle = open(filename, "r", encoding="utf8")
+   
+    # Prepare to read the data file as a CSV rather than just strings
+    csv_reader = DictReader(file_handle)
+
+    # Read each row of the CSV line-by-line
+    for row in csv_reader:
+        result.append(row)
+
+    # Close the file when you're done, to free its resources
+    file_handle.close()
+
+    return result
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Produce a list[str] with all values in a single column."""
+    result: list[str] = []
+    for row in table:
+        item: str = row[column]
+        result.append(item)
+    return result
+
+
+def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform a row-oriented table to a column-oriented table."""
+    result: dict[str, list[str]] = {}
+
+    first_row: dict[str, str] = row_table[0]
+    for column in first_row:
+        result[column] = column_values(row_table, column)
+    return result
+
+
+def head(column_table: dict[str, list[str]], n: int) -> dict[str, list[str]]:
+    """Produce a new column-based table with only the first N rows of data for each column."""
+    result: dict[str, list[str]] = {}
+    for column in column_table:
+        n_values = []
+        i = 0
+        while i < n:
+            value_of_n = column_table[column][i]
+            n_values.append(value_of_n)
+            i += 1
+        result[column] = n_values
+    return result
+
+
+def select(original_table: dict[str, list[str]], names: list[str]) -> dict[str, list[str]]:
+    """Produce a new column-based table with only a specific subset of the original columns."""
+    result: dict[str, list[str]] = {}
+    for name in names:
+        result[name] = original_table[name]
+    return result
+
+
+def concat(first_table: dict[str, list[str]], second_table: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Produce a new column-based table with two column-based tables combined."""
+    # I know this isn't correct, but it's the only thing that doesn't return an error 
+    result: dict[str, list[str]] = {}
+    for column in first_table:
+        result[column] = first_table[column]
+    for column in second_table:
+        result[column] = second_table[column]
+    return result
